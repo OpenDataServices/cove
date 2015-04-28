2b23c3314bbc5d996175859b83c5d0026016a020
Ben Webb <bjwebb67@googlemail.com>
2015-04-28 11:04:01 +0100

    Add a textarea to paste JSON into

--- datainput/views.py
+++ datainput/views.py
@@ -1,12 +1,12 @@
 from django.utils.translation import ugettext_lazy as _
 from django.shortcuts import render, redirect
-from django.forms import ModelForm
+from django import forms
 from datainput.models import SuppliedData
 from django.views.generic.edit import CreateView
 from django.core.files.base import ContentFile
 import requests
 
-class UploadForm(ModelForm):
+class UploadForm(forms.ModelForm):
      class Meta:
          model = SuppliedData
          fields = ['original_file']
@@ -14,7 +14,7 @@ class UploadForm(ModelForm):
             'original_file': _('Upload a file')
          }
 
-class UrlForm(ModelForm):
+class UrlForm(forms.ModelForm):
      class Meta:
          model = SuppliedData
          fields = ['source_url']
@@ -22,26 +22,37 @@ class UrlForm(ModelForm):
             'source_url': _('Supply a URL')
          }
 
+class TextForm(forms.Form):
+    paste = forms.CharField(label=_('Paste'), widget=forms.Textarea)
+
 # Create your views here.
 input = CreateView.as_view(model=SuppliedData, fields=['original_data'])
 def input(request):
     form_classes = {
         'upload_form': UploadForm,
         'url_form': UrlForm,
+        'text_form': TextForm,
     }
     forms = {form_name: form_class() for form_name, form_class in form_classes.items()}
     if request.method == 'POST':
         if 'source_url' in request.POST:
             form_name = 'url_form'
+        elif 'paste' in request.POST:
+            form_name = 'text_form'
         else:
             form_name = 'upload_form'
         form = form_classes[form_name](request.POST, request.FILES)
         forms[form_name] = form
         if form.is_valid():
-            data = form.save()
+            if form_name == 'text_form':
+                data = SuppliedData.objects.create()
+            else:
+                data = form.save()
             if form_name == 'url_form':
                 r = requests.get(data.source_url)
                 # FIXME!!!! - test.json is a bad name, and this loads into memory
                 data.original_file.save('test.json', ContentFile(r.content))
+            elif form_name == 'text_form':
+                data.original_file.save('test.json', ContentFile(form['paste'].value()))
             return redirect(data.get_absolute_url())
     return render(request, 'datainput/input.html', {'forms':forms})
