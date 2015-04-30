fce5c530247c6633e4340102026a0c7301d1c204
Ben Webb <bjwebb67@googlemail.com>
2015-04-30 14:48:58 +0100

    Allow functional tests to be run against a custom url

--- fts/tests.py
+++ fts/tests.py
@@ -1,6 +1,7 @@
 import pytest
 from selenium import webdriver
 import time
+import os
 
 
 @pytest.fixture(scope="module")
@@ -11,13 +12,21 @@ def browser(request):
     return browser
 
 
-def test_index_page(live_server, browser):
-    browser.get(live_server.url + '/')
+@pytest.fixture(scope="module")
+def server_url(request, live_server):
+    if 'CUSTOM_SERVER_URL' in os.environ:
+        return os.environ['CUSTOM_SERVER_URL']
+    else:
+        return live_server.url
+
+
+def test_index_page(server_url, browser):
+    browser.get(server_url + '/')
     assert 'Open Contracting Data Explorer' in browser.find_element_by_tag_name('body').text
 
 
-def test_accordion(live_server, browser):
-    browser.get(live_server.url + '/')
+def test_accordion(server_url, browser):
+    browser.get(server_url + '/')
 
     def buttons():
         return [b.is_displayed() for b in browser.find_elements_by_tag_name('button')]
