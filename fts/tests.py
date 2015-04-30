fc61e06c1248167c3ab4abe5a4dd33d704584939
Ben Webb <bjwebb67@googlemail.com>
2015-04-30 14:05:16 +0100

    Add accordion functional test

--- fts/tests.py
+++ fts/tests.py
@@ -1,5 +1,6 @@
 import pytest
 from selenium import webdriver
+import time
 
 
 @pytest.fixture(scope="module")
@@ -13,3 +14,20 @@ def browser(request):
 def test_index_page(live_server, browser):
     browser.get(live_server.url + '/')
     assert 'Open Contracting Data Explorer' in browser.find_element_by_tag_name('body').text
+
+
+def test_accordion(live_server, browser):
+    browser.get(live_server.url + '/')
+
+    def buttons():
+        return [b.is_displayed() for b in browser.find_elements_by_tag_name('button')]
+
+    time.sleep(0.5)
+    assert buttons() == [True, False, False]
+    browser.find_element_by_partial_link_text('Link').click()
+    browser.implicitly_wait(1)
+    time.sleep(0.5)
+    assert buttons() == [False, True, False]
+    browser.find_element_by_partial_link_text('Paste').click()
+    time.sleep(0.5)
+    assert buttons() == [False, False, True]
