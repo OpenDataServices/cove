5969c57daf9ffda8fc08b7068e61a28ad6e70191
David Carpenter <caprenter@gmail.com>
2015-06-04 13:08:18 +0100

    [#12][#38] Adds terms and conditions page to the site(s)
    Generic terms and conditions for all cove sites

--- cove/urls.py
+++ cove/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url
+from django.views.generic import TemplateView
 
 urlpatterns = [
     url(r'^$', 'cove.input.views.input', name='index'),
     url(r'^data/(.+)$', 'cove.views.explore', name='explore'),
+    url(r'^terms', TemplateView.as_view(template_name='terms.html'), name='terms'),
 ]
