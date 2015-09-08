d369428c40c406608c9e30b520bc5696ff3ca388
Ben Webb <bjwebb67@googlemail.com>
2015-09-08 11:56:30 +0100

    Fix test by rearranging urls.py
    Opened https://github.com/OpenDataServices/cove/issues/96 to fix this in
    a cleaner way (by modifying the tests)

--- cove/urls_multi.py
+++ cove/urls_multi.py
@@ -9,9 +9,9 @@ urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^i18n/', include('django.conf.urls.i18n')),
 
+    url(r'^resourceprojects/', include('cove.urls', namespace='cove-resourceprojects', app_name='cove')),
     url(r'^ocds/', include('cove.urls', namespace='cove-ocds', app_name='cove')),
     url(r'^360/', include('cove.urls', namespace='cove-360', app_name='cove')),
-    url(r'^resourceprojects/', include('cove.urls', namespace='cove-resourceprojects', app_name='cove')),
     url(r'^$', TemplateView.as_view(template_name='multi_index.html'), name='multi_index'),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
