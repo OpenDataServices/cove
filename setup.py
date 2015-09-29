a457663cd2b568c66ee794e5c28df144ce0f9c62
Ben Webb <ben.webb@opendataservices.coop>
2015-09-29 16:50:40 +0100

    Add setup.py
    Required to pull cove in via pip:
    https://github.com/NRGI/resource-projects-etl/issues/40

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+
+install_requires = []
+dependency_links = []
+
+with open('./requirements.txt') as requirements_txt:
+    requirements = requirements_txt.read().strip().splitlines()
+    for requirement in requirements:
+        if requirement.startswith('#'):
+            continue
+        elif requirement.startswith('-e '):
+            install_requires.append(requirement.split('=')[1])
+            dependency_links.append(requirement[3:])
+        else:
+            install_requires.append(requirement)
+
+setup(
+    name='cove',
+    version='0.0.0',
+    author='Open Data Services',
+    author_email='code@opendataservices.coop',
+    packages=['cove', 'cove.input', 'cove.dataload'],
+    scripts=['manage.py'],
+    url='https://github.com/OpenDataServices/cove',
+    description='',
+    classifiers=[
+        'License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)',
+    ],
+    install_requires=install_requires,
+    dependency_links=dependency_links
+)
