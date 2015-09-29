5ffe9b220751f11df885fbbcc9ca4600b1d8a1ee
Ben Webb <ben.webb@opendataservices.coop>
2015-09-29 17:21:34 +0100

    Remove dependency_links, as it doesn't work properly (and is deprecated)
    https://github.com/NRGI/resource-projects-etl/issues/40
    
    We can add the git url in resource-projects-etl's requirements.txt
    instead.

--- setup.py
+++ setup.py
@@ -1,7 +1,6 @@
 from setuptools import setup
 
 install_requires = []
-dependency_links = []
 
 with open('./requirements.txt') as requirements_txt:
     requirements = requirements_txt.read().strip().splitlines()
@@ -10,11 +9,8 @@ with open('./requirements.txt') as requirements_txt:
             continue
         elif requirement.startswith('-e '):
             install_requires.append(requirement.split('=')[1])
-            dependency_links.append(requirement[3:])
         else:
             install_requires.append(requirement)
-
-print(install_requires)
 
 setup(
     name='cove',
@@ -29,5 +25,4 @@ setup(
         'License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)',
     ],
     install_requires=install_requires,
-    dependency_links=dependency_links
 )
