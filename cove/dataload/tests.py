496989e7b557055909947c8a1cd2aae540e98648
Ben Webb <ben.webb@opendataservices.coop>
2015-10-01 12:03:29 +0100

    Refactor dataload tests slightly

--- cove/dataload/tests.py
+++ cove/dataload/tests.py
@@ -10,12 +10,13 @@ PROCESSES = OrderedDict([
     ('nodeps', {
         'name': '',
         'depends': None,
-        'main': True
+        'main': True,
+        'reverse_id': 'nodeps_reverse',
     }),
-    ('notmain', {
+    ('nodeps_reverse', {
         'name': '',
         'depends': '',
-        'main': False
+        'main': False,
     })
 ])
 cove_resourceprojects_mock = mock.Mock(PROCESSES=PROCESSES)
@@ -32,16 +33,26 @@ with mock.patch('builtins.__import__', side_effect=import_mock):
 
 
 @pytest.mark.django_db
-def test_statuses(settings):
-    list_statuses = list(v.statuses(m.Dataset()))
-    assert len(list_statuses) == 1
-    assert list_statuses[0]['label_class'] == ''
-    assert list_statuses[0]['last_run'] is None
+class TestStatuses:
+    def test_no_process_runs(self):
+        list_statuses = list(v.statuses(m.Dataset()))
+        assert len(list_statuses) == 1
+        assert list_statuses[0]['label_class'] == ''
+        assert list_statuses[0]['last_run'] is None
 
-    supplied_data = cove.input.models.SuppliedData.objects.create()
-    dataset = m.Dataset.objects.create(supplied_data=supplied_data)
-    last_run = m.ProcessRun.objects.create(dataset=dataset, process='nodeps', successful=True)
-    list_statuses = list(v.statuses(dataset))
-    assert len(list_statuses) == 1
-    assert list_statuses[0]['label_class'] == 'label-success'
-    assert list_statuses[0]['last_run'] == last_run
+    @pytest.mark.parametrize('successful', [True, False])
+    def test_last_run(self, successful):
+        supplied_data = cove.input.models.SuppliedData.objects.create()
+        dataset = m.Dataset.objects.create(supplied_data=supplied_data)
+        # Create some process runs
+        m.ProcessRun.objects.create(dataset=dataset, process='nodeps', successful=True)
+        m.ProcessRun.objects.create(dataset=dataset, process='nodeps', successful=False)
+        # Assign the last one to a variable, and check later that's what we get returned
+        last_run = m.ProcessRun.objects.create(dataset=dataset, process='nodeps', successful=successful)
+        list_statuses = list(v.statuses(dataset))
+        assert len(list_statuses) == 1
+        if successful:
+            assert list_statuses[0]['label_class'] == 'label-success'
+        else:
+            assert list_statuses[0]['label_class'] == 'label-danger'
+        assert list_statuses[0]['last_run'] == last_run
