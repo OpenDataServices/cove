b6b0c48d1346f005d19c86b6645e412780813aca
Ben Webb <ben.webb@opendataservices.coop>
2015-10-01 10:28:38 +0100

    Remove dataload from main urls.py

--- cove/urls.py
+++ cove/urls.py
@@ -4,9 +4,6 @@ from django.views.generic import TemplateView
 urlpatterns = [
     url(r'^$', 'cove.input.views.input', name='index'),
     url(r'^data/(.+)$', 'cove.views.explore', name='explore'),
-    url(r'^dataload/$', 'cove.dataload.views.dataload', name='dataload'),
-    url(r'^dataload/([^/]+)/$', 'cove.dataload.views.dataset', name='dataload_dataset'),
-    url(r'^dataload/([^/]+)/([^/]+)$', 'cove.dataload.views.run_process', name='dataload_dataset'),
     url(r'^terms', TemplateView.as_view(template_name='terms.html'), name='terms'),
     url(r'^stats', 'cove.views.stats', name='stats'),
 ]
