adb27922714331302d3e27d8891da3a62da48d1b
David Raznick <kindly@gmail.com>
2016-06-06 15:12:05 +0100

    [#393] see all validation errors

--- cove/templatetags/cove_tags.py
+++ cove/templatetags/cove_tags.py
@@ -9,6 +9,11 @@ def cove_modal_list(**kw):
     return kw
 
 
+@register.inclusion_tag('modal_errors.html')
+def cove_modal_errors(**kw):
+    return kw
+
+
 @register.filter(name='get_message')
 def get_message(error):
     return json.loads(error)[1]
@@ -17,3 +22,8 @@ def get_message(error):
 @register.filter(name='get_message_type')
 def get_message_type(error):
     return json.loads(error)[0]
+
+
+@register.filter(name='concat')
+def concat(arg1, arg2):
+    return str(arg1) + str(arg2)
