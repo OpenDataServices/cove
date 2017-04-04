ff802e623637fdb986f5dbc28f42789833f84715
David Raznick <kindly@gmail.com>
2017-04-04 15:23:51 +0100

    [#629] Fix media url so not in /validator

--- cove_360/urls.py
+++ cove_360/urls.py
@@ -1,4 +1,6 @@
 from django.conf.urls import url
+from django.conf.urls.static import static
+from django.conf import settings
 
 from cove.urls import urlpatterns, handler500  # noqa: F401
 
@@ -7,3 +9,5 @@ urlpatterns += [
     url(r'^data/(.+)$', 'cove_360.views.explore_360', name='explore'),
     url(r'^common_errors', 'cove_360.views.common_errors', name='common_errors')
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- cove_ocds/urls.py
+++ cove_ocds/urls.py
@@ -1,12 +1,18 @@
 from django.conf.urls import url, include
 from django.views.generic import RedirectView
+from django.conf.urls.static import static
+from django.conf import settings
 
-from cove.urls import urlpatterns, handler500  # noqa: F401
+from cove.urls import urlpatterns as urlpatterns_core
+from cove.urls import handler500  # noqa: F401
 
-urlpatterns += [url(r'^data/(.+)$', 'cove_ocds.views.explore_ocds', name='explore')]
 
 # Serve the OCDS validator at /validator/
+urlpatterns_core += [url(r'^data/(.+)$', 'cove_ocds.views.explore_ocds', name='explore')]
+
 urlpatterns = [
     url(r'^$', RedirectView.as_view(url='validator/', permanent=False)),
-    url('^validator/', include(urlpatterns))
+    url('^validator/', include(urlpatterns_core)),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
