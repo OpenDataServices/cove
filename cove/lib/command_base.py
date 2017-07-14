9afd7166cfaa6ae7c43ecbe22712a5f14673c01c
David Raznick <kindly@gmail.com>
2017-07-14 16:51:17 +0100

    [#749] Further inital CLI work.
    Seperate out API.
    Add delete existing directory.
    Create wrapper script to make running easier.
    Put result.json in directory

--- cove/lib/command_base.py
+++ cove/lib/command_base.py
@@ -1,6 +1,7 @@
 from django.core.management.base import BaseCommand
 import os
 import sys
+import shutil
 
 
 class CoveCommandBase(BaseCommand):
@@ -8,17 +9,21 @@ class CoveCommandBase(BaseCommand):
 
     def add_arguments(self, parser):
         parser.add_argument('file', help='File Cove to process.')
-        parser.add_argument('--output-dir', default='', help='Directory where output is created, defaults to the name of the file')
+        parser.add_argument('--output-dir', '-o', default='', help='Directory where output is created, defaults to the name of the file')
+        parser.add_argument('--delete', '-d', action='store_true', help='Delete existing directory if it exits')
 
     def handle(self, file, *args, **options):
         output_dir = options.get("output_dir")
         if not output_dir:
             output_dir = file.split('/')[-1].split('.')[0]
 
-        try:
-            os.makedirs(output_dir)
-        except OSError:
-            self.stdout.write("Directory {} already exists".format(output_dir))
-            sys.exit(1)
+        if os.path.exists(output_dir):
+            if options['delete']:
+                shutil.rmtree(output_dir)
+            else:
+                self.stdout.write("Directory {} already exists".format(output_dir))
+                sys.exit(1)
+
+        os.makedirs(output_dir)
 
         self.output_dir = output_dir
--- cove_ocds/management/commands/cove_ocds.py
+++ cove_ocds/management/commands/cove_ocds.py
@@ -1,12 +1,21 @@
 from cove.lib.command_base import CoveCommandBase
 import json
+import os
+import sys
 from cove.lib.tools import get_file_type
 from cove_ocds.lib.schema import SchemaOCDS
 from cove_ocds.views import common_checks_ocds
 from cove.lib.common import get_spreadsheet_meta_data
 from cove.lib.converters import convert_spreadsheet, convert_json
+from cove_ocds.lib.api import produce_json_output, APIException
 import pprint
+import warnings
 
+class SetEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, set):
+            return list(obj)
+        return json.JSONEncoder.default(self, obj)
 
 class Command(CoveCommandBase):
     help = 'Run Command Line version of Cove OCDS'
@@ -18,53 +27,14 @@ class Command(CoveCommandBase):
     def handle(self, file, *args, **options):
         super().handle(file, *args, **options)
 
-        context = {}
-        file_type = get_file_type(file)
-        context = {"file_type": file_type}
+        try:
+            result = produce_json_output(self.output_dir, file)
+        except APIException as e:
+            self.stdout.write(str(e))
+            sys.exit(1)
 
-        if file_type == 'json':
-            with open(file, encoding='utf-8') as fp:
-                try:
-                    json_data = json.load(fp)
-                except ValueError as err:
-                    self.stdout.write('The file looks like invalid json')
+        with open(os.path.join(self.output_dir, "results.json"), 'w+') as result_file:
+            json.dump(result, result_file, indent=2, cls=SetEncoder)
 
-                schema_ocds = SchemaOCDS(release_data=json_data)
-                
-                if schema_ocds.extensions:
-                    schema_ocds.create_extended_release_schema_file(self.output_dir, "")
 
-                url = schema_ocds.extended_schema_file or schema_ocds.release_schema_url
 
-                context.update(convert_json(self.output_dir, '', file, schema_url=url, flatten=True))
-
-        else:
-            metatab_schema_url = SchemaOCDS(select_version='1.1').release_pkg_schema_url
-            metatab_data = get_spreadsheet_meta_data(self.output_dir, file, metatab_schema_url, file_type=file_type)
-
-            schema_ocds = SchemaOCDS(release_data=metatab_data)
-
-            #if schema_ocds.invalid_version_data:
-            #    raise_invalid_version_data(metatab_data.get('version'))
-
-            # Replace json conversion when user chooses a different schema version.
-            #if db_data.schema_version and schema_ocds.version != db_data.schema_version:
-            #    replace = True
-
-            if schema_ocds.extensions:
-                schema_ocds.create_extended_release_schema_file(self.output_dir, '')
-            url = schema_ocds.extended_schema_file or schema_ocds.release_schema_url
-            pkg_url = schema_ocds.release_pkg_schema_url
-
-            context.update(convert_spreadsheet(self.output_dir, '', file, file_type, schema_url=url, pkg_schema_url=pkg_url))
-
-            with open(context['converted_path'], encoding='utf-8') as fp:
-                json_data = json.load(fp)
-
-        #if replace:
-        #    if os.path.exists(validation_errors_path):
-        #        os.remove(validation_errors_path)
-
-        context = common_checks_ocds(context, self.output_dir, json_data, schema_ocds)
-
-        pprint.pprint(context)
