f3de8f2480be8235dce31d8be4a90ffe317d97b9
Eduardo Gomez <eduardo.gomez@hotmail.co.uk>
2017-07-17 16:38:17 +0100

    [#749] cove_ocds_cli: pass option to exclude original data from result folder

--- cove/lib/command_base.py
+++ cove/lib/command_base.py
@@ -8,12 +8,13 @@ class CoveCommandBase(BaseCommand):
     help = 'Run Command Line version of Cove'
 
     def add_arguments(self, parser):
-        parser.add_argument('file', help='File Cove to process.')
+        parser.add_argument('file', help='File to be processed by Cove')
         parser.add_argument('--output-dir', '-o', default='', help='Directory where output is created, defaults to the name of the file')
         parser.add_argument('--delete', '-d', action='store_true', help='Delete existing directory if it exits')
+        parser.add_argument('--exclude-file', '-e', action='store_true', help='Do not include the file in the output directory')
 
     def handle(self, file, *args, **options):
-        output_dir = options.get("output_dir")
+        output_dir = options.get('output-dir')
         if not output_dir:
             output_dir = file.split('/')[-1].split('.')[0]
 
@@ -21,7 +22,7 @@ class CoveCommandBase(BaseCommand):
             if options['delete']:
                 shutil.rmtree(output_dir)
             else:
-                self.stdout.write("Directory {} already exists".format(output_dir))
+                self.stdout.write('Directory {} already exists'.format(output_dir))
                 sys.exit(1)
 
         os.makedirs(output_dir)
--- cove_ocds/management/commands/cove_ocds.py
+++ cove_ocds/management/commands/cove_ocds.py
@@ -1,8 +1,10 @@
-from cove.lib.command_base import CoveCommandBase
 import json
 import os
 import sys
+from shutil import copy2
+
 from cove_ocds.lib.api import produce_json_output, APIException
+from cove.lib.command_base import CoveCommandBase
 
 
 class SetEncoder(json.JSONEncoder):
@@ -17,10 +19,11 @@ class Command(CoveCommandBase):
 
     def add_arguments(self, parser):
         super().add_arguments(parser)
-        parser.add_argument('--schema-version', default='', help='Version of schema to be used (only for OCDS)')
+        parser.add_argument('--schema-version', default='', help='Version of schema to be used')
 
     def handle(self, file, *args, **options):
         super().handle(file, *args, **options)
+        exclude_file = options.get('exclude-file')
 
         try:
             result = produce_json_output(self.output_dir, file)
@@ -30,3 +33,6 @@ class Command(CoveCommandBase):
 
         with open(os.path.join(self.output_dir, "results.json"), 'w+') as result_file:
             json.dump(result, result_file, indent=2, cls=SetEncoder)
+
+        if not exclude_file:
+            copy2(file, self.output_dir)
