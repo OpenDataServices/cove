25ba08ce40db00057d3da6c850159e25cd05fae0
Ben Webb <ben.webb@opendataservices.coop>
2017-07-19 13:03:06 +0100

    [#768] Handle errors with very bad 360Giving json data
    (ie. those without a list under the grants key)

--- cove_360/views.py
+++ cove_360/views.py
@@ -71,6 +71,12 @@ def explore_360(request, pk, template='cove_360/explore.html'):
             json_data = json.load(fp)
 
     context = common_checks_360(context, db_data, json_data, schema_360)
+
+    if hasattr(json_data, 'get') and hasattr(json_data.get('grants'), '__iter__'):
+        context['grants'] = json_data['grants']
+    else:
+        context['grants'] = []
+
     return render(request, template, context)
 
 
