c70ac2d682e84db8603ee5b29add6eb0554b8a6c
Ben Webb <ben.webb@opendataservices.coop>
2017-08-04 14:27:04 +0100

    Upgrade to latest Django LTS: 1.11

--- cove/urls.py
+++ cove/urls.py
@@ -6,6 +6,9 @@ from django.views.generic import TemplateView
 from django.template import Context, loader
 from django.http import HttpResponseServerError
 
+import cove.input.views
+import cove.views
+
 
 def handler500(request):
     """500 error handler which includes ``request`` in the context.
@@ -25,9 +28,9 @@ def cause500(request):
 
 
 urlpatterns = [
-    url(r'^$', 'cove.input.views.data_input', name='index'),
+    url(r'^$', cove.input.views.data_input, name='index'),
     url(r'^terms', TemplateView.as_view(template_name='terms.html'), name='terms'),
-    url(r'^stats', 'cove.views.stats', name='stats'),
+    url(r'^stats', cove.views.stats, name='stats'),
     url(r'^test/500', cause500),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^i18n/', include('django.conf.urls.i18n'))
--- cove_360/urls.py
+++ cove_360/urls.py
@@ -4,11 +4,13 @@ from django.conf import settings
 
 from cove.urls import urlpatterns, handler500  # noqa: F401
 
+import cove_360.views
+
 
 urlpatterns += [
-    url(r'^data/(.+)/advanced$', 'cove_360.views.explore_360', name='explore', kwargs=dict(template='cove_360/explore_advanced.html')),
-    url(r'^data/(.+)$', 'cove_360.views.explore_360', name='explore'),
-    url(r'^common_errors', 'cove_360.views.common_errors', name='common_errors')
+    url(r'^data/(.+)/advanced$', cove_360.views.explore_360, name='explore', kwargs=dict(template='cove_360/explore_advanced.html')),
+    url(r'^data/(.+)$', cove_360.views.explore_360, name='explore'),
+    url(r'^common_errors', cove_360.views.common_errors, name='common_errors')
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
