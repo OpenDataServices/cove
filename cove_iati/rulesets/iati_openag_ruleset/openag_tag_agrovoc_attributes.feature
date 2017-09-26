f64c4450a834beefd8ce620b20c9111264a026b5
Eduardo Gomez <eduardo.gomez@hotmail.co.uk>
2017-09-26 13:31:28 +0100

    [#834] iati_openag: add steps to check for right attributes in openag:tag

--- cove_iati/rulesets/iati_openag_ruleset/openag_tag_agrovoc_attributes.feature
+++ cove_iati/rulesets/iati_openag_ruleset/openag_tag_agrovoc_attributes.feature
@@ -4,6 +4,14 @@ Feature: openag:tag element checks
     Given an Open Agriculture IATI activity
      then at least one `openag:tag` is expected
 
-  Scenario Outline: openag-tag: must use Agrovoc
+  Scenario Outline: openag-tag: element must use vocabulary maintained by the Reporting Organisation
     Given openag:tag elements
-     then at least one `openag:tag` must use Agrovoc classification
+     then every `openag:tag` must have @vocabulary="98"|"99"
+
+  Scenario Outline: openag-tag: element must use Agrovoc vocabulary-uri
+    Given openag:tag elements
+     then every `openag:tag` must have @vocabulary-uri=="http://aims.fao.org/aos/agrovoc/"
+
+  Scenario Outline: openag-tag: element must use code attribute
+    Given openag:tag elements
+     then every `openag:tag` must have @code
--- cove_iati/rulesets/iati_openag_ruleset/steps/openag_ruleset_step_definitions.py
+++ cove_iati/rulesets/iati_openag_ruleset/steps/openag_ruleset_step_definitions.py
@@ -8,6 +8,12 @@ from behave import given, then
 from cove_iati.lib.exceptions import RuleSetStepException
 
 
+def get_xpaths(context, xpath_expression):
+    nsmap = context.xml.getparent().nsmap
+    xpaths = context.xml.xpath(xpath_expression, namespaces=nsmap)
+    return xpaths
+
+
 @given('an Open Agriculture IATI activity')
 def step_given_iati_activity(context):
     assert True
@@ -15,12 +21,12 @@ def step_given_iati_activity(context):
 
 @then('at least one `{xpath_expression}` is expected')
 def step_openag_expected(context, xpath_expression):
-    nsmap = context.xml.getparent().nsmap
-    vals = context.xml.xpath(xpath_expression, namespaces=nsmap)
-    if not vals:
+    xpaths = get_xpaths(context, xpath_expression)
+    if not xpaths:
+        tree = context.xml.getroottree()
         errors = [{
-            'message': 'the activity should have at least one `{}`'.format(xpath_expression),
-            'path': xpath_expression
+            'message': 'the activity should include at least one {} element'.format(xpath_expression),
+            'path': tree.getpath(context.xml)
         }]
         raise RuleSetStepException(context, errors)
 
@@ -30,28 +36,84 @@ def step_given_openag_tag(context):
     assert True
 
 
-@then('at least one `{}` must use Agrovoc classification')
-def step_openag_tag_with_agrovoc_classification(context, xpath_expression):
-    nsmap = context.xml.getparent().nsmap
-    values = context.xml.xpath(xpath_expression, namespaces=nsmap)
-    fail = True
+@then('every `{xpath_expression}` must have @vocabulary="98"|"99"')
+def step_openag_tag_vocabulary(context, xpath_expression):
+    xpaths = get_xpaths(context, xpath_expression)
+    fail = False
 
-    if values:
+    if xpaths:
         errors = []
         tree = context.xml.getroottree()
-        for val in values:
-            attributes = val.attrib
-            attr_vocabulary = attributes.get('vocabulary') == '98' or attributes.get('vocabulary') == '99'
-            attr_vocabulary_uri = attributes.get('vocabulary-uri') == 'http://aims.fao.org/aos/agrovoc/'
-            attr_code = attributes.get('code')
-            if attr_vocabulary and attr_vocabulary_uri and attr_code:
-                fail = False
-                break
-        else:
-            errors.append({
-                'message': 'There should be at least one openag:tag using Agrovoc classification',
-                'path': tree.getpath(val.getparent())
-            })
+        for xpath in xpaths:
+            attrib = xpath.attrib
+            vocabulary = attrib.get('vocabulary')
+            org_vocabulary = attrib.get('vocabulary') == '98' or attrib.get('vocabulary') == '99'
 
-        if fail:
-            raise RuleSetStepException(context, errors)
+            if vocabulary and org_vocabulary:
+                continue
+            else:
+                if not vocabulary:
+                    msg = '{} element must have a vocabulary attribute'.format(xpath_expression)
+                elif not org_vocabulary:
+                    msg = '{} element must have code "98" or "99" for the vocabulary attribute (it is "{}")'
+                    msg = msg.format(xpath_expression, attrib.get('vocabulary'))
+
+                errors.append({'message': msg, 'path': tree.getpath(xpath)})
+                fail = True
+
+    if fail:
+        raise RuleSetStepException(context, errors)
+
+
+@then('every `{xpath_expression}` must have @vocabulary-uri=="http://aims.fao.org/aos/agrovoc/"')
+def step_openag_tag_vocabulary_uri(context, xpath_expression):
+    xpaths = get_xpaths(context, xpath_expression)
+    fail = False
+
+    if xpaths:
+        errors = []
+        tree = context.xml.getroottree()
+        for xpath in xpaths:
+            attrib = xpath.attrib
+            vocabulary_uri = attrib.get('vocabulary-uri')
+            agrovoc_uri = attrib.get('vocabulary-uri') == 'http://aims.fao.org/aos/agrovoc/'
+
+            if vocabulary_uri and agrovoc_uri:
+                continue
+            else:
+                if not vocabulary_uri:
+                    msg = '{} element must have a vocabulary-uri attribute'.format(xpath_expression)
+                elif not agrovoc_uri:
+                    msg = ('{} element must have "http://aims.fao.org/aos/agrovoc/" '
+                           'uri for the vocabulary-uri attribute (it is "{}")')
+                    msg = msg.format(xpath_expression, attrib.get('vocabulary-uri'))
+
+                errors.append({'message': msg, 'path': tree.getpath(xpath)})
+                fail = True
+
+    if fail:
+        raise RuleSetStepException(context, errors)
+
+
+@then('every `{xpath_expression}` must have @code')
+def step_openag_tag_code(context, xpath_expression):
+    xpaths = get_xpaths(context, xpath_expression)
+    fail = False
+
+    if xpaths:
+        errors = []
+        tree = context.xml.getroottree()
+        for xpath in xpaths:
+            attrib = xpath.attrib
+            code_attrib = attrib.get('code')
+
+            if code_attrib:
+                continue
+            else:
+                if not code_attrib:
+                    msg = '{} element must have a code attribute'.format(xpath_expression)
+                    errors.append({'message': msg, 'path': tree.getpath(xpath)})
+                    fail = True
+
+    if fail:
+        raise RuleSetStepException(context, errors)
