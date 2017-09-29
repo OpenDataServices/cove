56c0766dbf157dc5bf4049facb83c216079a4087
Eduardo Gomez <eduardo.gomez@hotmail.co.uk>
2017-09-29 23:47:08 +0100

    [#834] iati_ruleset: use register_ruleset_errors decorator

--- cove_iati/rulesets/iati_openag_ruleset/steps/openag_ruleset_step_definitions.py
+++ cove_iati/rulesets/iati_openag_ruleset/steps/openag_ruleset_step_definitions.py
@@ -6,92 +6,86 @@ License: https://github.com/pwyf/bdd-tester/blob/master/LICENSE
 from behave import then
 
 from cove.lib.common import get_orgids_prefixes
-from cove_iati.lib.exceptions import RuleSetStepException
-from cove_iati.rulesets.utils import get_xpaths, get_full_xpath
+from cove_iati.rulesets.utils import get_full_xpath, get_xpaths, register_ruleset_errors
 
 ORGIDS_PREFIXES = get_orgids_prefixes()
 
 
 @then('at least one `{xpath_expression}` element is expected')
+@register_ruleset_errors
 def step_openag_expected(context, xpath_expression):
     xpaths = get_xpaths(context.xml, xpath_expression)
+    errors = []
+
     if not xpaths:
         errors = [{
             'message': 'the activity should include at least one <{}> element'.format(xpath_expression),
             'path': get_full_xpath(context.xml, context.xml)
         }]
-        raise RuleSetStepException(context, errors)
+    return context, errors
 
 
 @then('every `{xpath_expression}` must have `{attribute}` attribute')
+@register_ruleset_errors
 def step_openag_tag_attribute_expected(context, xpath_expression, attribute):
     xpaths = get_xpaths(context.xml, xpath_expression)
-    msg = '{} element must have @{} attribute'
-    fail = False
+    errors = []
 
     if xpaths:
-        errors = []
+        msg = '{} element must have @{} attribute'
         for xpath in xpaths:
             attrib = xpath.attrib
             required_attrib = attrib.get(attribute)
             if not required_attrib:
                     errors.append({'message': msg.format(xpath_expression, attribute),
                                    'path': get_full_xpath(context.xml, xpath)})
-                    fail = True
-    if fail:
-        raise RuleSetStepException(context, errors)
+    return context, errors
 
 
 @then('every `{attribute}` must be equal to `{any_value}`')
+@register_ruleset_errors
 def step_openag_tag_attribute_accepted_values(context, attribute, any_value):
     xpaths = get_xpaths(context.xml, context.xpath_expression)
-    msg = '"{}" is not a valid value for @{} attribute (it should be "{}")'
-    fail = False
+    errors = []
 
     if xpaths:
-        errors = []
+        msg = '"{}" is not a valid value for @{} attribute (it should be "{}")'
         for xpath in xpaths:
             element_attrs = xpath.attrib
             matching_value = any([element_attrs.get(attribute) == val for val in any_value.split(' or ')])
             if not matching_value:
                 errors.append({'message': msg.format(element_attrs.get(attribute), attribute, any_value),
                                'path': '{}/@{}'.format(get_full_xpath(context.xml, xpath), attribute)})
-                fail = True
-    if fail:
-        raise RuleSetStepException(context, errors)
+    return context, errors
 
 
 @then('every `{xpath_expression1}` must include `{xpath_expression2}` element')
+@register_ruleset_errors
 def step_openag_location_id_expected(context, xpath_expression1, xpath_expression2):
     xpaths = get_xpaths(context.xml, xpath_expression1)
-    msg = '{} must contain a {} element'
-    fail = False
+    errors = []
 
     if xpaths:
-        errors = []
+        msg = '{} must contain a {} element'
         for xpath in xpaths:
             has_xpath_expression2 = xpath.find(xpath_expression2)
             if not has_xpath_expression2:
                 errors.append({'message': msg.format(xpath_expression1, xpath_expression2),
                                'path': get_full_xpath(context.xml, xpath)})
-                fail = True
-    if fail:
-        raise RuleSetStepException(context, errors)
+    return context, errors
 
 
 @then('`{attribute}` id attribute should start with an org-ids prefix')
+@register_ruleset_errors
 def step_openag_org_id_prefix_expected(context, attribute):
     xpaths = get_xpaths(context.xml, context.xpath_expression)
-    msg = '@{} {} does not start with a recognised org-ids prefix'
-    fail = False
+    errors = []
 
     if xpaths:
-        errors = []
+        msg = '@{} {} does not start with a recognised org-ids prefix'
         for xpath in xpaths:
             attr_id = xpath.attrib.get(attribute, '')
             if attr_id[:6].upper() not in ORGIDS_PREFIXES:
                 errors.append({'message': msg.format(attr_id, attribute),
                                'path': '{}/@{}'.format(get_full_xpath(context.xml, xpath), attribute)})
-                fail = True
-    if fail:
-        raise RuleSetStepException(context, errors)
+    return context, errors
--- cove_iati/rulesets/iati_standard_v2_ruleset/steps/standard_ruleset_step_definitions.py
+++ cove_iati/rulesets/iati_standard_v2_ruleset/steps/standard_ruleset_step_definitions.py
@@ -8,16 +8,7 @@ import re
 
 from behave import given, then
 
-from cove_iati.lib.exceptions import RuleSetStepException
-from cove_iati.rulesets.utils import get_xpaths, get_full_xpath
-
-
-def invalid_date_format(xpath, date_str):
-    try:
-        datetime.datetime.strptime(date_str, '%Y-%m-%d')
-    except ValueError:
-        return True
-    return False
+from cove_iati.rulesets.utils import invalid_date_format, get_full_xpath, get_xpaths, register_ruleset_errors
 
 
 @given('an IATI activity')
@@ -42,29 +33,27 @@ def step_given_two_different_elements(context, xpath_expression1, xpath_expressi
 
 
 @then('`{attribute}` attribute must be a valid date')
+@register_ruleset_errors
 def step_valid_date(context, attribute):
     xpaths = get_xpaths(context.xml, context.xpath_expression)
-    fail = False
+    errors = []
 
     if xpaths:
-        errors = []
         for xpath in xpaths:
             date_str = xpath.attrib.get(attribute)
             if invalid_date_format(xpath, date_str):
                 errors.append({'message': '`{}` is not a valid date'.format(date_str),
                                'path': '{}/@{}'.format(get_full_xpath(context.xml, xpath), attribute)})
-                fail = True
-    if fail:
-        raise RuleSetStepException(context, errors)
+    return context, errors
 
 
 @then('`{attribute}` attribute must be today or in the past')
+@register_ruleset_errors
 def step_must_be_today_or_past(context, attribute):
     xpaths = get_xpaths(context.xml, context.xpath_expression)
-    fail = False
+    errors = []
 
     if xpaths:
-        errors = []
         today = datetime.date.today()
         for xpath in xpaths:
             date_str = xpath.attrib.get(attribute)
@@ -72,76 +61,77 @@ def step_must_be_today_or_past(context, attribute):
             if date > today:
                 errors.append({'message': '{} must be on or before today ({})'.format(date, today),
                                'path': '{}/@{}'.format(get_full_xpath(context.xml, xpath), attribute)})
-                fail = True
-    if fail:
-        raise RuleSetStepException(context, errors)
+    return context, errors
 
 
 @then('iati-identifier text should match the regex `{regex_str}`')
+@register_ruleset_errors
 def step_iati_id_text_match_regex(context, regex_str):
     xpath = get_xpaths(context.xml, context.xpath_expression)
     regex = re.compile(regex_str)
+    errors = []
 
     if xpath:
         xpath = xpath[0]
+        fail_msg = 'text does not match the regex {}'
         text_str = xpath.text
         if not regex.match(text_str):
-
-            fail_msg = 'text does not match the regex {}'
             errors = [{'message': fail_msg.format(text_str, regex_str),
                        'path': '{}/text()'.format(get_full_xpath(context.xml, xpath))}]
-            raise RuleSetStepException(context, errors)
+    return context, errors
 
 
 @then('`{attribute}` attribute should match the regex `{regex_str}`')
+@register_ruleset_errors
 def step_attribute_match_regex(context, attribute, regex_str):
     xpaths = get_xpaths(context.xml, context.xpath_expression)
     regex = re.compile(regex_str)
-    fail = False
+    errors = []
 
     if xpaths:
-        errors = []
         fail_msg = '{} does not match the regex {}'
         for xpath in xpaths:
             attr_str = xpath.attrib.get(attribute)
             if not regex.match(attr_str):
                 errors.append({'message': fail_msg.format(attr_str, regex_str),
                                'path': '{}/@{}'.format(get_full_xpath(context.xml, xpath), attribute)})
-            fail = True
-    if fail:
-        raise RuleSetStepException(context, errors)
+    return context, errors
 
 
 @then('either `{xpath_expression1}` or `{xpath_expression2}` is expected')
+@register_ruleset_errors
 def step_either_or_expected(context, xpath_expression1, xpath_expression2):
     xpath = get_xpaths(context.xml, xpath_expression1) or get_xpaths(context.xml, xpath_expression2)
+    errors = []
+
     if not xpath:
         fail_msg = 'Neither {} nor {} have been found'
         errors = [{'message': fail_msg.format(xpath_expression1, xpath_expression2),
                    'path': get_full_xpath(context.xml, context.xml)}]
-        raise RuleSetStepException(context, errors)
+    return context, errors
 
 
 @then('`{xpath_expression}` is not expected')
+@register_ruleset_errors
 def step_should_not_be_present(context, xpath_expression):
     xpaths = get_xpaths(context.xml, xpath_expression)
-    fail = False
+    errors = []
 
     if xpaths:
-        errors = []
         for xpath in xpaths:
             errors.append({'message': '`{}` is not expected'.format(xpath_expression),
                            'path': get_full_xpath(context.xml, xpath)})
-    if fail:
-        raise RuleSetStepException(context, errors)
+    return context, errors
 
 
 @then('both `{attribute}` attributes must be a valid date')
+@register_ruleset_errors
 def step_two_valid_dates(context, attribute):
     xpath1 = get_xpaths(context.xml, context.xpath_expression1)
     xpath2 = get_xpaths(context.xml, context.xpath_expression2)
     xpaths = []
-    fail = False
+    errors = []
+
     if xpath1:
         xpaths.append(xpath1[0])
     if xpath2:
@@ -149,23 +139,23 @@ def step_two_valid_dates(context, attribute):
 
     if xpaths:
         fail_msg = '`{}` is not a valid date'
-        errors = []
         for xpath in xpaths:
             date_str = xpath.attrib.get(attribute)
             if invalid_date_format(xpath1, date_str):
                 errors.append({'message': fail_msg.format(date_str),
                                'path': '{}/@{}'.format(get_full_xpath(context.xml, xpath), attribute)})
-                fail = True
-    if fail:
-        raise RuleSetStepException(context, errors)
+    return context, errors
 
 
 @then('`{attribute}` start date attribute must be chronologically before end date attribute')
+@register_ruleset_errors
 def step_should_be_before(context, attribute):
     xpath1 = get_xpaths(context.xml, context.xpath_expression1)
     xpath2 = get_xpaths(context.xml, context.xpath_expression2)
+    errors = []
+
     if not xpath1 or not xpath2:
-        return
+        return context, errors
 
     xpath1 = xpath1[0]
     start_date_str = xpath1.attrib.get(attribute)
@@ -173,13 +163,13 @@ def step_should_be_before(context, attribute):
     xpath2 = xpath2[0]
     end_date_str = xpath2.attrib.get(attribute)
     end_date = datetime.strptime(end_date_str, '%Y-%m-%d').date()
-    fail_msg = 'start date ({}) must be before end date ({})'
 
     if start_date >= end_date:
+        fail_msg = 'start date ({}) must be before end date ({})'
         path_str1 = '{}/@{}'.format(get_full_xpath(context.xml, xpath1), attribute)
         path_str2 = '{}/@{}'.format(get_full_xpath(context.xml, xpath2), attribute)
         errors = [{
             'message': fail_msg.format(start_date_str, end_date_str),
             'path': '{} & {}'.format(path_str1, path_str2)
         }]
-        raise RuleSetStepException(context, errors)
+    return context, errors
