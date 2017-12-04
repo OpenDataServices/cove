0314665971b8134af9baa94509c5f4d75e389cc9
Eduardo Gomez <eduardo.gomez@hotmail.co.uk>
2017-12-04 12:08:44 +0000

    [#899] Add rule: feature_name to RuleSetStepException

--- cove_iati/lib/exceptions.py
+++ cove_iati/lib/exceptions.py
@@ -5,6 +5,7 @@ class RuleSetStepException(Exception):
     def __init__(self, context, errors=''):
         self.errors = errors
         self.id = ''
+        self.feature_name = context.feature.name
         try:
             self.id = context.xml.xpath('iati-identifier/text()')[0]
         except:
@@ -14,4 +15,5 @@ class RuleSetStepException(Exception):
         return json.dumps({
             'errors': self.errors,
             'id': self.id,
+            'rule': self.feature_name
         })
