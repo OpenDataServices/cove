1a092ba66c2c56e6ceff202f61cdbd5711af2698
Yohanna Lisnichuk <yohanitalisnichuk@gmail.com>
2017-12-13 14:15:39 -0300

    folder option fixes
    Signed-off-by: Yohanna Lisnichuk <yohanitalisnichuk@gmail.com>

--- cove_ocds/management/commands/ocds_cli.py
+++ cove_ocds/management/commands/ocds_cli.py
@@ -32,19 +32,17 @@ class Command(CoveBaseCommand):
 
         try:
             if os.path.isdir(file):
-                with open(os.path.join(self.output_dir, "all_results.txt"), 'w+') as outfile:
-                    for f in os.listdir(file):
-                        filename = os.fsdecode(f)
-                        if filename.endswith(".json"):
-                            result = ocds_json_output(self.output_dir, os.path.join(file, filename), schema_version,
-                                                      convert)
-                            result.update({'file': filename})
-                            json.dump(result, outfile, cls=SetEncoder)
-                            outfile.write("\n")
+                with open(os.path.join(self.output_dir, 'all_results.txt'), 'w+') as outfile:
+                    for filename in os.listdir(file):
+                        result = ocds_json_output(self.output_dir, os.path.join(file, filename), schema_version,
+                                                  convert)
+                        result.update({'file': filename})
+                        json.dump(result, outfile, cls=SetEncoder)
+                        outfile.write('\n')
                     outfile.close()
             else:
                 result = ocds_json_output(self.output_dir, file, schema_version, convert)
-                with open(os.path.join(self.output_dir, "results.json"), 'w+') as result_file:
+                with open(os.path.join(self.output_dir, 'results.json'), 'w+') as result_file:
                     json.dump(result, result_file, indent=2, cls=SetEncoder)
         except APIException as e:
             self.stdout.write(str(e))
