528f05538f0b6d8d088a5d346594aec7d19fdbb9
Ben Webb <ben.webb@opendataservices.coop>
2018-01-29 15:55:57 +0000

    [#948] Run tests on travis against secret data

---
+++ secret_data_test.py
@@ -0,0 +1,15 @@
+import os
+import json
+import glob
+import subprocess
+
+os.makedirs('secret_data_test', exist_ok=True)
+os.chdir('secret_data_test')
+
+for fname in glob.glob('../ocds/*'):
+    subprocess.call(['../ocds-cli', fname])
+
+for dirname in os.listdir('.'):
+    with open(os.path.join(dirname, 'results.json')) as fp, open(os.path.join('..', 'secret_data_test_archive', dirname, 'results.json')) as fp_archive:
+
+        assert json.load(fp) == json.load(fp_archive)
