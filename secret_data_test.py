7f39d8d6c5cbe3df3bb75459995c2848f0caa7cd
Ben Webb <ben.webb@opendataservices.coop>
2018-02-01 14:24:30 +0000

    Add a docstring to secret_data_test.py

--- secret_data_test.py
+++ secret_data_test.py
@@ -3,6 +3,16 @@ import json
 import glob
 import subprocess
 
+"""
+This is a test that uses old/unpublished data files that we don't want to make
+public.
+It checks that the JSON produced by ocds-cli is the same as previously.
+
+Travis is set up with a password to download the files from a password
+protected url, see .travis.yml
+
+"""
+
 os.makedirs('secret_data_test', exist_ok=True)
 os.chdir('secret_data_test')
 
