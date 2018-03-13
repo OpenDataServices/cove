32a01ef55c21ef75ec0bc5ec887b13c162a98be8
Ben Webb <ben.webb@opendataservices.coop>
2018-03-13 14:57:26 +0000

    Fix flake8

--- secret_data_test.py
+++ secret_data_test.py
@@ -26,7 +26,7 @@ for fname in glob.glob('../../ocds/*'):
 
 for dirname in os.listdir('.'):
     print(dirname)
-    with open(os.path.join(dirname, 'results.json')) as fp, open(os.path.join('..', '..','secret_data_test_archive', 'ocds', dirname, 'results.json')) as fp_archive:
+    with open(os.path.join(dirname, 'results.json')) as fp, open(os.path.join('..', '..', 'secret_data_test_archive', 'ocds', dirname, 'results.json')) as fp_archive:
         assert json.load(fp) == json.load(fp_archive)
 
 os.chdir('../..')
