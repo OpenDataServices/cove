1f8361c84dcd95b0f8b9b19233815a7fb436afba
Ben Webb <ben.webb@opendataservices.coop>
2018-04-24 16:42:19 +0100

    Add script to fetch secret data
    This ensures third party pull requests don't fail at the install stage.

---
+++ get_secret_data.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+wget http://travis:$TRAVIS_SECRET_FILES_PASSWORD@travis-secret-files.live3.default.opendataservices.uk0.bigv.io/ocdstestdata.zip
+unzip ocdstestdata.zip
+wget http://travis:$TRAVIS_SECRET_FILES_PASSWORD@travis-secret-files.live3.default.opendataservices.uk0.bigv.io/360testdata.zip
+unzip 360testdata.zip
+wget http://travis:$TRAVIS_SECRET_FILES_PASSWORD@travis-secret-files.live3.default.opendataservices.uk0.bigv.io/secret_data_test_archive_6.zip
+unzip secret_data_test_archive_6.zip
