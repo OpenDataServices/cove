4d563dc2710252d573ca62c3c86c6753f3a7d4fa
Ben Webb <ben.webb@opendataservices.coop>
2018-05-23 10:40:40 +0100

    Update Privacy Policy with necessary info for GDPR

--- cove_ocds/urls.py
+++ cove_ocds/urls.py
@@ -15,7 +15,6 @@ urlpatterns_core += [url(r'^data/(.+)$', cove_ocds.views.explore_ocds, name='exp
 urlpatterns = [
     url(r'^$', RedirectView.as_view(url='validator/', permanent=False)),
     url('^validator/', include(urlpatterns_core)),
-    url(r'^validator/terms_ocds/$', TemplateView.as_view(template_name='terms_ocds.html'), name='terms'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
