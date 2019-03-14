a2459c69012bbc70e4fb8211564c30a3ae4f3454
Ben Webb <ben.webb@opendataservices.coop>
2019-03-14 15:14:10 +0900

    Update secret data test to 8

--- get_secret_data.sh
+++ get_secret_data.sh
@@ -3,5 +3,5 @@ wget http://travis:$TRAVIS_SECRET_FILES_PASSWORD@travis-secret-files.live3.defau
 unzip ocdstestdata.zip
 wget http://travis:$TRAVIS_SECRET_FILES_PASSWORD@travis-secret-files.live3.default.opendataservices.uk0.bigv.io/360testdata.zip
 unzip 360testdata.zip
-wget http://travis:$TRAVIS_SECRET_FILES_PASSWORD@travis-secret-files.live3.default.opendataservices.uk0.bigv.io/secret_data_test_archive_10.zip
-unzip secret_data_test_archive_10.zip
+wget http://travis:$TRAVIS_SECRET_FILES_PASSWORD@travis-secret-files.live3.default.opendataservices.uk0.bigv.io/secret_data_test_archive_8.zip
+unzip secret_data_test_archive_8.zip
