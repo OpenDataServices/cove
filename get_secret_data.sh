0f6d1225dc28886cf642ba1a2108c4f2a2a184cf
Bibiana Cristofol Amat <bcristofol@gmail.com>
2019-05-13 11:29:31 +0100

    Update secret data test archive file to 10

--- get_secret_data.sh
+++ get_secret_data.sh
@@ -4,4 +4,4 @@ unzip ocdstestdata.zip
 wget http://travis:$TRAVIS_SECRET_FILES_PASSWORD@travis-secret-files.live3.default.opendataservices.uk0.bigv.io/360testdata.zip
 unzip 360testdata.zip
 wget http://travis:$TRAVIS_SECRET_FILES_PASSWORD@travis-secret-files.live3.default.opendataservices.uk0.bigv.io/secret_data_test_archive_9.zip
-unzip secret_data_test_archive_9.zip
+unzip secret_data_test_archive_10.zip
