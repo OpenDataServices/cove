c5fc6529b4b8b993db2ea547659c84532650e952
Rob Redpath <rob.redpath@opendataservices.coop>
2019-06-21 13:37:36 +0000

    Update SC message£

--- cove_360/lib/threesixtygiving.py
+++ cove_360/lib/threesixtygiving.py
@@ -617,10 +617,10 @@ class RecipientOrgCompanyNumber(AdditionalTest):
     }
     check_text['message'][(0, 100)] = mark_safe(
         "Common causes of this are missing leading digits, typos or incorrect values "
-        "being entered into this field. Company numbers are typically 8 digits, possibly starting SC, "
-        "for example <span class=\"highlight-background-text\">SC01234569</span> or "
-        "<span class=\"highlight-background-text\">09876543</span>. You can check company numbers online "
-        "at <a href=\"https://beta.companieshouse.gov.uk/\">Companies House</a>."
+        "being entered into this field. Company numbers are typically 8 digits, "
+        "for example <span class=\"highlight-background-text\">09876543</span> or sometimes start with a "
+        "2 letter prefix, <span class=\"highlight-background-text\">SC123459</span>. You can check "
+        "company numbers online at <a href=\"https://beta.companieshouse.gov.uk/\">Companies House</a>."
     )
 
     def process(self, grant, path_prefix):
