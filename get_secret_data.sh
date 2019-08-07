007f6048bd512a65beaee6ba750bedbc3ed766df
James Baster <james.baster@opendataservices.coop>
2019-08-07 12:49:10 +0100

    Remove OCDS Secret Data Tests https://github.com/OpenDataServices/cove/issues/1192

--- get_secret_data.sh
+++ get_secret_data.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-wget http://travis:$TRAVIS_SECRET_FILES_PASSWORD@travis-secret-files.live3.default.opendataservices.uk0.bigv.io/ocdstestdata.zip
-unzip ocdstestdata.zip
 wget http://travis:$TRAVIS_SECRET_FILES_PASSWORD@travis-secret-files.live3.default.opendataservices.uk0.bigv.io/360testdata.zip
 unzip 360testdata.zip
 wget http://travis:$TRAVIS_SECRET_FILES_PASSWORD@travis-secret-files.live3.default.opendataservices.uk0.bigv.io/secret_data_test_archive_12.zip
