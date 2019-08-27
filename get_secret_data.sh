7f0d1041b1239c3ebd50f8cb369b1ddb5594ff2b
James Baster <james.baster@opendataservices.coop>
2019-08-27 09:58:57 +0100

    Remove 360 Secret Data Tests

--- get_secret_data.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-wget http://travis:$TRAVIS_SECRET_FILES_PASSWORD@travis-secret-files.live3.default.opendataservices.uk0.bigv.io/360testdata.zip
-unzip 360testdata.zip
-wget http://travis:$TRAVIS_SECRET_FILES_PASSWORD@travis-secret-files.live3.default.opendataservices.uk0.bigv.io/secret_data_test_archive_12.zip
-unzip secret_data_test_archive_12.zip
