bf24d17ce592a7ad562d68f2ea7e0a039b9baf2d
Ben Webb <ben.webb@opendataservices.coop>
2019-10-31 13:13:18 +0000

    [#887] Write a custom message for utf-8 JSON errors

--- cove_ocds/views.py
+++ cove_ocds/views.py
@@ -65,6 +65,16 @@ def explore_ocds(request, pk):
         with open(file_name, encoding='utf-8') as fp:
             try:
                 json_data = json.load(fp, parse_float=Decimal, object_pairs_hook=OrderedDict)
+            except UnicodeError as err:
+                raise CoveInputDataError(context={
+                    'sub_title': _("Sorry, we can't process that data"),
+                    'link': 'index',
+                    'link_text': _('Try Again'),
+                    'msg': _(format_html("The file that you uploaded doesn't appear to be well formed JSON. OCDS JSON follows the I-JSON format, which requires UTF-8 encoding. Ensure that your file uses UTF-8 encoding, then try uploading again."
+                             '\n\n<span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true">'
+                             '</span> <strong>Error message:</strong> {}', err)),
+                    'error': format(err)
+                })
             except ValueError as err:
                 raise CoveInputDataError(context={
                     'sub_title': _("Sorry, we can't process that data"),
