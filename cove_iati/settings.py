44c943144b9b4bbded61458a7e5d2cefbe80f64a
Ben Webb <ben.webb@opendataservices.coop>
2021-01-29 13:51:58 +0000

    Remove defusedexpat dependency, for compatibility with recent pythons
    This is still secure because xmltodict, the lib we depedend on that uses
    pyexpat, ensures that entities don't get loaded by default.
    https://github.com/martinblech/xmltodict/blob/ae19c452ca000bf243bfc16274c060bf3bf7cf51/xmltodict.py#L356-L366

--- cove_iati/settings.py
+++ cove_iati/settings.py
@@ -1,12 +1,5 @@
-import sys
 import os
 import environ
-if "pytest" not in sys.modules:
-    # Check that we can import defusedexpat, as this will protect us against
-    # some XML attacks in xmltodict
-    # Needs a noqa comment as we don't actually use it here
-    import defusedexpat  # noqa: F401
-    pass
 
 # Needs a noqa comment to come after the above import
 from cove import settings  # noqa: E408
