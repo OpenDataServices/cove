cec0c97f382fee2b270e8dab43c1aafe02f3db8f
Michael Wood <michael.wood@opendataservices.coop>
2021-05-24 15:17:57 +0100

    cove_360: Remove from shared Open Data Services CoVE repository
    360Giving's CoVE has been separated out into it's own application at
    https://github.com/threesixtygiving/dataquality/

--- run_tests.sh
+++ run_tests.sh
@@ -1,3 +1,2 @@
 DJANGO_SETTINGS_MODULE=cove.settings py.test cove --cov --cov-report= $@
-DJANGO_SETTINGS_MODULE=cove_360.settings py.test cove_360 --cov-append --cov --cov-report= $@
 DJANGO_SETTINGS_MODULE=cove_iati.settings py.test cove_iati --cov-append --cov $@
